933ef9e6ef2fd62684c8550d137e0f9a40c8c07d
denzkie <denzkie14@hotmail.com>
2024-04-07 14:54:48 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
+import 'package:provider/provider.dart';
+import 'models/models.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      scrollBehavior: CustomScrollBehavior(),
+      // 1
+      home: MultiProvider(
+        providers: [
+          // 2
+          ChangeNotifierProvider(create: (context) => TabManager()),
+          // TODO: Add GroceryManager Provider
+          ChangeNotifierProvider(create: (context) => GroceryManager()),
+        ],
+        child: const Home(),
+      ),
+    );
+  }
+}
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
+}
